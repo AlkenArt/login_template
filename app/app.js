1c8cc49e860f8bccec321a1d3fda7ea47de00e80
Lokesh Kumar Nayak <lokeshkumar.nayak@arris.com>
2015-09-29 23:05:26 +0530

    add app

---
+++ app/app.js
@@ -0,0 +1,31 @@
+var login_template=angular.module('login_template', ['ngRoute']);
+login_template.controller('loginController',loginController);
+
+login_template.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when("/login", { templateUrl: "view/login.html", controller : "loginController"})
+      .when("/home", { templateUrl: "view/home.html", controller : "partialViewController"})
+      .otherwise( {redirectTo: '/login'});
+  }]);
+
+function loginController($scope, $location){
+	$scope.user='';
+	$scope.user.username='';
+	$scope.user.password='';
+	$scope.loginError=false;
+	$scope.submit = function(pathurl){
+			if($scope.user.username=="sample" && $scope.user.password=="sample")
+        	{
+        	 //console.log($scope.user);
+			 $location.path(pathurl);
+			 //$rootScope.loggedInUser = $scope.user.username;
+			 //$cookieStore.put('userId',$scope.user.username);
+        	}
+			else
+        	{
+        		$scope.loginError=true;
+        		//alert("wrong username or password");
+        	}
+		}
+	
+};
