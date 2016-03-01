282722b9acb29a30d63fbacbd7f94819b9c9b000
Lokesh Kumar Nayak <lokeshkumar.nayak@arris.com>
2016-03-01 23:19:05 +0530

    integrated with regEx for email pre-validation and optional with directive

--- app/app.js
+++ app/app.js
@@ -8,60 +8,62 @@ login_template.config(['$routeProvider','$locationProvider',function($routeProvi
       .otherwise( {redirectTo: '/login'});
   }]);
 
-login_template.directive('validateEmail', function() {
-	  var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-	  return {
-	    link: function(scope, elm) {
-	      elm.on("keyup",function(){
-	            var isMatchRegex = EMAIL_REGEXP.test(elm.val());
-	            if( isMatchRegex&& elm.hasClass('warning') || elm.val() == ''){
-	              elm.removeClass('warning');
-	            }else if(isMatchRegex == false && !elm.hasClass('warning')){
-	              elm.addClass('warning');
-	            }
-	      });
-	    }
-	  }
-	});
+//Below code has used for email validation through "validate-email" attribute
+//login_template.directive('validateEmail', function() {
+//	  var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+//	  return {
+//	    link: function(scope, elm) {
+//	      elm.on("keyup",function(){
+//	            var isMatchRegex = EMAIL_REGEXP.test(elm.val());
+//	            if( isMatchRegex&& elm.hasClass('warning') || elm.val() == ''){
+//	              elm.removeClass('warning');
+//	            }else if(isMatchRegex == false && !elm.hasClass('warning')){
+//	              elm.addClass('warning');
+//	            }
+//	      });
+//	    }
+//	  }
+//	});
 
 function loginController($scope, $location, $http){
 	$scope.user='';
 	$scope.user.username='';
 	$scope.user.password='';
-	$scope.email = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
 	$scope.loginError=false;
+	$scope.validateEmail=function(email){
+		var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+		if(!EMAIL_REGEXP.test(email)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 	$scope.submit = function(pathurl){
-<<<<<<< HEAD
-			$http.get('http://localhost:8080/login_template/data/user.json')
-			.success(function(data){
-				if($scope.user.username==data.user.username && $scope.user.password==data.user.password)
-	        	{
-	        	 //console.log($scope.user);
-				 $location.path(pathurl);
-				 //$rootScope.loggedInUser = $scope.user.username;
-				 //$cookieStore.put('userId',$scope.user.username);
-	        	}
-				else
-	        	{
-	        		$scope.loginError=true;
-	        		//alert("wrong username or password");
-	        	}
-			})
-			.error(function(data, status){
-				$scope.errormessage="User list is not available";
-			});
-=======
-			if($scope.user.username=="sample" && $scope.user.password=="sample")
-        	{
-        	 //console.log($scope.user);
-			 $location.path(pathurl);
-        	}
-			else
-        	{
-        		$scope.loginError=true;
-        		//alert("wrong username or password");
-        	}
->>>>>>> 69abc16b1c735a50063c25fcaa1fada9306f73d9
+			if($scope.validateEmail($scope.user.username)){
+				$http.get('http://localhost:8080/login_template/data/user.json')
+				.success(function(data){
+					if($scope.user.username==data.user.username && $scope.user.password==data.user.password)
+		        	{
+		        	 //console.log($scope.user);
+					 $location.path(pathurl);
+					 //$rootScope.loggedInUser = $scope.user.username;
+					 //$cookieStore.put('userId',$scope.user.username);
+		        	}
+					else
+		        	{
+		        		$scope.loginError=true;
+		        		$scope.errorMessage="Username or Password is Invalid";
+		        	}
+				})
+				.error(function(data, status){
+					$scope.errorMessage="Not able to connect with the server, please try again";
+				});
+			}
+			else{
+				$scope.loginError=true;
+				$scope.errorMessage="Please enter valid format of username"
+			}
 		}
 	
 };
